c209c055ad20dbad9a8684399d9eec283143ebb8
Evgeny Sazhnev <yegres98@gmail.com>
2021-03-12 12:53:13 +0300

    updated setup.py and added method

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="missile-env",
+    version='0.0.1',
+    desctiption="Private package for guided missile model",
+    url='git@github.com:Yegres5/missile-env.git',
+    author='Evgeny Sazhnev',
+    author_email='yegres98@gmail.com',
+    license='unlicense'
+)
