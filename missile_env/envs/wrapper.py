a2fe100faa75489e4f430945029bfcc8a553e765
Evgeny Sazhnev <yegres98@gmail.com>
2021-04-22 10:00:46 +0300

    Reward changed

--- missile_env/envs/wrapper.py
+++ missile_env/envs/wrapper.py
@@ -107,10 +107,10 @@ class Wrapper:
         #         self.reward_program = np.delete(self.reward_program, 0, axis=0)
 
 
-        # reward += -0.1
-        reward = 0
+        reward += -0.1
+        # reward = 0
         if self.rocket.destroyed:
-            reward += -6
+            reward += -5
         elif self.rocket.targetLost():
             reward += -10
 
